4995fc86853b17184ec4fb5fdb5e9103335e16c7
debmallya <debmallyapathak2017@gmail.com>
2024-02-02 00:12:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const selectAgainst=(sel)=>{
+    if(sel==='ai')
+     {
+         window.location.href='withAi.html'
+        return ;
+     }   
+     window.location.href='withPlayer.html'
+  
+ }
+
